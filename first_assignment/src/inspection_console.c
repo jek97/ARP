c7fe188dc75b67898e45abcadef905a3bbdf6c6a
jek97 <jek.lugano@yahoo.com>
2022-12-14 14:04:05 +0100

    i've started the code by writing in a pipe all the pid of the proces from the master to the inspect, that then i will use in the signals

--- first_assignment/src/inspection_console.c
+++ first_assignment/src/inspection_console.c
@@ -8,24 +8,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int ic; // declaring control variable
+int jc; // declaring control variable
+int kc; // declaring control variable
+
+int ic1; // declaring control variable
+int jc1; // declaring control variable
+int kc1; // declaring control variable
+
 int x_ins_in; // declare the file descriptor of the pipe x_c
 int z_ins_in; // declare the file descriptor of the pipe z_c
 char *x_c = "/named_pipes/x_c"; // initialize the pipe x_c pathname
 char *z_c = "/named_pipes/z_c"; // initialize the pipe z_c pathname
 
+int pid_cmd_dim_fd; // declare the file descriptor of the pipe pid_cmd_dim
+int pid_m1_dim_fd; // declare the file descriptor of the pipe pid_m1_dim
+int pid_m2_dim_fd; // declare the file descriptor of the pipe pid_m2_dim
+char *pid_cmd_dim_name = "/named_pipes/pid_cmd_dim"; // initialize the pipe pid_cmd_dim pathname
+char *pid_m1_dim_name = "/named_pipes/pid_m1_dim"; // initialize the pipe pid_m1_dim pathname
+char *pid_m2_dim_name = "/named_pipes/pid_m2_dim"; // initialize the pipe pid_m2_dim pathname
+
+int pid_cmd_fd; // declare the file descriptor of the pipe pid_cmd
+int pid_m1_fd; // declare the file descriptor of the pipe pid_m1
+int pid_m2_fd; // declare the file descriptor of the pipe pid_m2
+char *pid_cmd_name = "/named_pipes/pid_cmd"; // initialize the pipe pid_cmd pathname
+char *pid_m1_name = "/named_pipes/pid_m1"; // initialize the pipe pid_m1 pathname
+char *pid_m2_name = "/named_pipes/pid_m2"; // initialize the pipe pid_m2 pathname
+
 int x_rcv[1]; // declare the x position receiving buffer
 int z_rcv[1]; // declare the z position receiving buffer
+int pid_cmd_dim_rcv[]; // declare the cmd pid dimension receiving buffer
+int pid_m1_dim_rcv[]; // declare the m1 pid dimension receiving buffer
+int pid_m2_dim_rcv[]; // declare the m2 pid dimension receiving buffer
+
+//int pid_cmd_rcv[]; // declare the cmd pid receiving buffer
+//int pid_m1_rcv[]; // declare the m1 pid receiving buffer
+//int pid_m2_rcv[]; // declare the m2 pid receiving buffer
 
 fd_set rfds; // declare the select mode
 struct timeval tv; // declare the time interval of the select function
-int retval; // declare the returned valeu
-int nfds = 1; // initialize number of fd starting from 0
+int retvale; // declare the returned valeu
+int nfds = 2; // initialize number of fd starting from 0
 int fd; // declare the counter for FD_ISSET
+
+fd_set rfds1; // declare the select mode
+struct timeval tv1; // declare the time interval of the select function
+int retvale1; // declare the returned valeu
+int nfds1 = 2; // initialize number of fd starting from 0
+int fd1; // declare the counter for FD_ISSET
+
+fd_set rfds2; // declare the select mode
+struct timeval tv2; // declare the time interval of the select function
+int retvale2; // declare the returned valeu
+int nfds2 = 1; // initialize number of fd starting from 0
+int fd2; // declare the counter for FD_ISSET
 
 float x, z; // End-effector coordinates
 
-int main(int argc, char const *argv[])
-{
+int pid_cmd; // declare the variable where i will store the pid of the cmd
+int pid_m1; // declare the variable where i will store the pid of the m1
+int pid_m2; // declare the variable where i will store the pid of the m2
+
+int main(int argc, char const *argv[]){
     // Utility variable to avoid trigger resize event on launch
     int first_resize = TRUE;
 
@@ -41,29 +85,161 @@ int main(int argc, char const *argv[])
         perror("error opening the pipe z_c from inspection"); // checking errors
     }
 
+    // pid dimension communication pipes:
+    pid_cmd_dim_fd = open(pid_cmd_dim_name, O_RDONLY); // open the pipe pid_cmd_dim to read from it
+    if(pid_cmd_dim_fd < 0){
+        perror("error opening the pipe pid_cmd_dim from inspection"); // checking errors
+    }
+
+    pid_m1_dim_fd = open(pid_m1_dim_name, O_RDONLY); // open the pipe pid_m1_dim to read from it
+    if(pid_m1_dim_fd < 0){
+        perror("error opening the pipe pid_m1_dim from inspection"); // checking errors
+    }
+
+    pid_m2_dim_fd = open(pid_m2_dim_name, O_RDONLY); // open the pipe pid_m2_dim to read from it
+    if(pid_m2_dim_fd < 0){
+        perror("error opening the pipe pid_m2_dim from inspection"); // checking errors
+    }
+
+    // pid communication pipes:
+    pid_cmd_fd = open(pid_cmd_name, O_RDONLY); // open the pipe pid_cmd to read from it
+    if(pid_cmd_fd < 0){
+        perror("error opening the pipe pid_cmd from inspection"); // checking errors
+    }
+
+    pid_m1_fd = open(pid_m1_name, O_RDONLY); // open the pipe pid_m1 to read from it
+    if(pid_m1_fd < 0){
+        perror("error opening the pipe pid_m1 from inspection"); // checking errors
+    }
+
+    pid_m2_fd = open(pid_m2_name, O_RDONLY); // open the pipe pid_m2 to read from it
+    if(pid_m2_fd < 0){
+        perror("error opening the pipe pid_m2 from inspection"); // checking errors
+    }
+
     // Initialize User Interface 
     init_console_ui();
+
+    // loop to obtain the pid dimension:
+    while((ic+jc+kc)<3){
+        // setting up the select
+        FD_ZERO(&rfds); // clear all the fd in the set
+        FD_SET(pid_cmd_dim_fd, &rfds); // put the fd of pid_cmd_dim_fd in the set
+        FD_SET(pid_m1_dim_fd, &rfds); // put the fd of pid_m1_dim_fd in the set
+        FD_SET(pid_m2_dim_fd, &rfds); // put the fd of pid_m2_dim_fd in the set
+        tv.tv_sec = 1; // set the time interval in seconds
+        tv.tv_usec = 0; // set the time interval in microseconds
+
+        if (select((nfds+1), &rfds, NULL, NULL, &tv) < 0) { // checking errors
+            perror("error in the pid dimension select of inspect");
+        }
+        else if (select((nfds+1), &rfds, NULL, NULL, &tv) == 0){
+            perror("timer elapsed in inspect pid dimension select without data");
+        }
+        else{
+            for (fd = 0; fd <= nfds; fd++) {
+                if (FD_ISSET(fd, &rfds)) { // checking if there is a pipe readyfor communications
+                    if (fd == pid_cmd_dim_fd) { // the pipe pid_cmd_dim_fd is ready for communicate
+                        if(read(pid_cmd_dim_fd, pid_cmd_dim_rcv, sizeof(pid_cmd_dim_rcv)) < 0) { // checking errors
+                            perror("error reading the pipe pid_cmd_dim from inspect"); 
+                        }
+                        else if (read(pid_cmd_dim_fd, pid_cmd_dim_rcv, sizeof(pid_cmd_dim_rcv)) > 0) { // otherwise read the pid dimension of cmd
+                            int pid_cmd_rcv[pid_cmd_dim_rcv[0]]; // declare the cmd pid receiving buffer, look at the end of the program
+                            ic = 1;
+                        }
+                    }
+                    else if (fd == pid_m1_dim_fd) { // the pipe pid_m1_dim_fd is ready for communicate
+                        if(read(pid_m1_dim_fd, pid_m1_dim_rcv, sizeof(pid_m1_dim_rcv)) < 0) { // checking errors
+                            perror("error reading the pipe pid_m1_dim from inspect"); 
+                        }
+                        else if (read(pid_m1_dim_fd, pid_m1_dim_rcv, sizeof(pid_m1_dim_rcv)) > 0) { // otherwise read the pid dimension of m1
+                            int pid_m1_rcv[pid_m1_dim_rcv[0]]; // declare the m1 pid receiving buffer, look at the end of the program
+                            jc = 1;
+                        }
+                    }
+                    else if (fd == pid_m2_dim_fd) { // the pipe pid_m2_dim_fd is ready for communicate
+                        if(read(pid_m2_dim_fd, pid_m2_dim_rcv, sizeof(pid_m2_dim_rcv)) < 0) { // checking errors
+                            perror("error reading the pipe pid_m2_dim from inspect"); 
+                        }
+                        else if (read(pid_m2_dim_fd, pid_m2_dim_rcv, sizeof(pid_m2_dim_rcv)) > 0) { // otherwise read the pid dimension of m2
+                            int pid_m2_rcv[pid_m2_dim_rcv[0]]; // declare the m2 pid receiving buffer, look at the end of the program
+                            kc = 1;
+                        }
+                    }
+                }
+            }
+    }
+
+    // loop to set the pid of the processes:
+    while((ic1+jc1+kc1)<3){
+        // setting up the select
+        FD_ZERO(&rfds1); // clear all the fd in the set
+        FD_SET(pid_cmd_fd, &rfds1); // put the fd of pid_cmd_fd in the set
+        FD_SET(pid_m1_fd, &rfds1); // put the fd of pid_m1_fd in the set
+        FD_SET(pid_m2_fd, &rfds1); // put the fd of pid_m2_fd in the set
+        tv.tv_sec = 1; // set the time interval in seconds
+        tv.tv_usec = 0; // set the time interval in microseconds
+
+        if (select((nfds1+1), &rfds1, NULL, NULL, &tv1) < 0) { // checking errors
+            perror("error in the pid select of inspect");
+        }
+        else if (select((nfds1+1), &rfds1, NULL, NULL, &tv1) == 0){
+            perror("timer elapsed in inspect pid select without data");
+        }
+        else{
+            for (fd1 = 0; fd1 <= nfds1; fd1++) {
+                if (FD_ISSET(fd1, &rfds1)) { // checking if there is a pipe ready for communications
+                    if (fd1 == pid_cmd_fd) { // the pipe pid_cmd_fd is ready for communicate
+                        if(read(pid_cmd_fd, pid_cmd_rcv, sizeof(pid_cmd_rcv)) < 0) { // checking errors
+                            perror("error reading the pipe pid_cmd from inspect"); 
+                        }
+                        else if (read(pid_cmd_fd, pid_cmd_rcv, sizeof(pid_cmd_rcv)) > 0) { // otherwise read the pid of cmd
+                            pid_cmd = pid_cmd_rcv[]; // probably here i will need to concatenate the array elements
+                            ic = 1;
+                        }
+                    }
+                    else if (fd1 == pid_m1_fd) { // the pipe pid_m1_fd is ready for communicate
+                        if(read(pid_m1_fd, pid_m1_rcv, sizeof(pid_m1_rcv)) < 0) { // checking errors
+                            perror("error reading the pipe pid_m1 from inspect"); 
+                        }
+                        else if (read(pid_m1_fd, pid_m1_rcv, sizeof(pid_m1_rcv)) > 0) { // otherwise read the pid of m1
+                            pid_m1 = pid_m1_rcv[]; // probably here i will need to concatenate the array elements
+                            jc = 1;
+                        }
+                    }
+                    else if (fd1 == pid_m2_fd) { // the pipe pid_m2_fd is ready for communicate
+                        if(read(pid_m2_fd, pid_m2_rcv, sizeof(pid_m2_rcv)) < 0) { // checking errors
+                            perror("error reading the pipe pid_m2 from inspect"); 
+                        }
+                        else if (read(pid_m2_fd, pid_m2_rcv, sizeof(pid_m2_rcv)) > 0) { // otherwise read the pid of m2
+                            pid_m2 = pid_m2_rcv[]; // probably here i will need to concatenate the array elements
+                            kc = 1;
+                        }
+                    }
+                }
+            }
+    }
 
     // Infinite loop
     while(TRUE)	{
 
         // setting up the select
-        FD_ZERO(&rfds); // clear all the fd in the set
-        FD_SET(x_ins_in, &rfds); // put the fd of x_ins_in in the set
-        FD_SET(z_ins_in, &rfds); // put the fd of z_ins_in in the set
-        tv.tv_sec = 5; // set the time interval in seconds
-        tv.tv_usec = 0; // set the time interval in microseconds
+        FD_ZERO(&rfds2); // clear all the fd in the set
+        FD_SET(x_ins_in, &rfds2); // put the fd of x_ins_in in the set
+        FD_SET(z_ins_in, &rfds2); // put the fd of z_ins_in in the set
+        tv2.tv2_sec = 5; // set the time interval in seconds
+        tv2.tv2_usec = 0; // set the time interval in microseconds
 
-        if (select((nfds+1), &rfds, NULL, NULL, &tv) < 0) { // checking errors
+        if (select((nfds2+1), &rfds2, NULL, NULL, &tv2) < 0) { // checking errors
             perror("error in the select of inspect");
         }
-        else if (select((nfds+1), &rfds, NULL, NULL, &tv) == 0){
+        else if (select((nfds2+1), &rfds2, NULL, NULL, &tv2) == 0){
             perror("timer elapsed in inspect select without data");
         }
         else{
-            for (fd = 0; fd <= nfds; fd++) {
-                if (FD_ISSET(fd, &rfds)) { // checking if there is a pipe readyfor communications
-                    if (fd == x_ins_in) { // the pipe x_ins_in is ready for communicate
+            for (fd2 = 0; fd2 <= nfds2; fd2++) {
+                if (FD_ISSET(fd2, &rfds2)) { // checking if there is a pipe readyfor communications
+                    if (fd2 == x_ins_in) { // the pipe x_ins_in is ready for communicate
                         if(read(x_ins_in, x_rcv, sizeof(x_rcv)) < 0) { // checking errors
                             perror("error reading the pipe x_c from inspect"); 
                         }
@@ -71,7 +247,7 @@ int main(int argc, char const *argv[])
                             x = x_rcv[0];
                         }
                     }
-                    else if (fd == z_ins_in) { // the pipe z_ins_in is ready for communicate
+                    else if (fd2 == z_ins_in) { // the pipe z_ins_in is ready for communicate
                         if(read(z_ins_in, z_rcv, sizeof(z_rcv)) < 0) { // checking errors
                             perror("error reading the pipe z_c from inspect"); 
                         }
@@ -150,3 +326,8 @@ int main(int argc, char const *argv[])
     endwin();
     return 0;
 }
+
+/* for solving the concatenation of the numbers in the pid array
+for(i=0; i<= sizeof(pid_proces_rcv)) {
+    pid_proces = pid_proces + pid_proces_rcv[i] * 10^i;
+}*/
--- first_assignment/src/master.c
+++ first_assignment/src/master.c
@@ -4,6 +4,8 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
 
 int mkpipe(const char *pathname, mode_t mode) { // function to create a named pipe
   int pipe; // declare the returned valeu of the funtion
@@ -57,8 +59,47 @@ int main() {
 
   char *err_z_c_ins = "./named_pipes/z_c"; // pathname of the pipe from error to inspect through z_c
   mode_t err_z_c_ins_mode = 0666; // mode of the pipe from error to inspect through z_c
+
+  // % from master to inspect, to communicate the pid of the processes command, m1, m2:
+  // % % pipe to communicate the pid dimension:
+  char *mas_pid_cmd_dim_ins = "./named_pipes/pid_cmd_dim"; // pathname of the pipe from master to inspect through pid_cmd_dim
+  mode_t mas_pid_cmd_dim_ins_mode = 0666; // mode of the pipe from master to inspect through pid_cmd_dim
+
+  char *mas_pid_m1_dim_ins = "./named_pipes/pid_m1_dim"; // pathname of the pipe from master to inspect through pid_m1_dim
+  mode_t mas_pid_m1_dim_ins_mode = 0666; // mode of the pipe from master to inspect through pid_m1_dim
+
+  char *mas_pid_m2_dim_ins = "./named_pipes/pid_m2_dim"; // pathname of the pipe from master to inspect through pid_m2_dim
+  mode_t mas_pid_m2_dim_ins_mode = 0666; // mode of the pipe from master to inspect through pid_m2_dim
+
+  // % % pipe to actually communicate the pid:
+  char *mas_pid_cmd_ins = "./named_pipes/pid_cmd"; // pathname of the pipe from master to inspect through pid_cmd
+  mode_t mas_pid_cmd_ins_mode = 0666; // mode of the pipe from master to inspect through pid_cmd
+
+  char *mas_pid_m1_ins = "./named_pipes/pid_m1"; // pathname of the pipe from master to inspect through pid_m1
+  mode_t mas_pid_m1_ins_mode = 0666; // mode of the pipe from master to inspect through pid_cmd
+
+  char *mas_pid_m2_ins = "./named_pipes/pid_m2"; // pathname of the pipe from master to inspect through pid_m2
+  mode_t mas_pid_m2_ins_mode = 0666; // mode of the pipe from master to inspect through pid_cmd
+
+  // declaring the fd of the needed pipes:
+  int fd_pid_cmd;
+  int fd_pid_m1;
+  int fd_pid_m2;
+  int fd_pid_cmd_dim;
+  int fd_pid_m1_dim;
+  int fd_pid_m2_dim;
+
+  // declaring the array where i will store the dimension of the pid:
+  int cmd_dim[1];
+  int m1_dim[1];
+  int m2_dim[1];
+
+  // declaring the array where i will store the pid to send them:
+  int cmd[];
+  int m1[];
+  int m2[];
   
-  // opening the named pipes:
+  // creating the named pipes:
   mkpipe(cmd_Vx_m1, cmd_Vx_m1_mode); // creating the named pipe for communicate the speed along x between the cmd and the motor1 
   mkpipe(cmd_Vz_m2, cmd_Vz_m2_mode); // creating the named pipe for communicate the speed along z between the cmd and the motor2
 
@@ -67,6 +108,14 @@ int main() {
 
   mkpipe(err_x_c_ins, err_x_c_ins_mode); // creating the named pipe for communicate the corrected position along x_c between the error and the inspect
   mkpipe(err_z_c_ins, err_z_c_ins_mode); // creating the named pipe for communicate the corrected position along z_c between the error and the inspect
+
+  mkpipe(mas_pid_cmd_dim_ins, mas_pid_cmd_dim_ins_mode); // creating the named pipe for communicate the dimension of the pid of the cmd between the master and the inspect
+  mkpipe(mas_pid_m1_dim_ins, mas_pid_m1_dim_ins_mode); // creating the named pipe for communicate the the dimension of the pid of the m1 between the master and the inspect
+  mkpipe(mas_pid_m2_dim_ins, mas_pid_m2_dim_ins_mode); // creating the named pipe for communicate the the dimension of the pid of the m2 between the master and the inspect
+
+  mkpipe(mas_pid_cmd_ins, mas_pid_cmd_ins_mode); // creating the named pipe for communicate the pid of the cmd between the master and the inspect
+  mkpipe(mas_pid_m1_ins, mas_pid_m1_ins_mode); // creating the named pipe for communicate the pid of the m1 between the master and the inspect
+  mkpipe(mas_pid_m2_ins, mas_pid_m2_ins_mode); // creating the named pipe for communicate the pid of the m2 between the master and the inspect
  
   // declaring the spawn() arguments:
   char * arg_list_command[] = { "/usr/bin/konsole", "-e", "./bin/command", NULL };
@@ -80,6 +129,77 @@ int main() {
   pid_t pid_m2 = spawn("/usr/bin/motor2", arg_list_m2);
   pid_t pid_err = spawn("/usr/bin/error", arg_list_err);
   pid_t pid_insp = spawn("/usr/bin/konsole", arg_list_insp);
+
+
+  // open the pipes from master to inspect for the pids dimension:
+  fd_pid_cmd_dim = open(mas_pid_cmd_dim_ins, O_WRONLY); // open the pipe mas_pid_cmd_dim_ins to write on it
+    if(fd_pid_cmd_dim < 0){
+        perror("error opening the pipe mas_pid_cmd_dim_ins from master"); // checking errors
+    }
+
+  fd_pid_m1_dim = open(mas_pid_m1_dim_ins, O_WRONLY); // open the pipe mas_pid_m1_dim_ins to write on it
+    if(fd_pid_m1_dim < 0){
+        perror("error opening the pipe mas_pid_m1_dim_ins from master"); // checking errors
+    }
+
+  fd_pid_m2_dim = open(mas_pid_m2_dim_ins, O_WRONLY); // open the pipe mas_pid_m2_dim_ins to write on it
+    if(fd_pid_m2_dim < 0){
+        perror("error opening the pipe mas_pid_m2_dim_ins from master"); // checking errors
+    }
+  
+  // open the pipes from master to inspect for the pids:
+  fd_pid_cmd = open(mas_pid_cmd_ins, O_WRONLY); // open the pipe mas_pid_cmd_ins to write on it
+    if(fd_pid_cmd < 0){
+        perror("error opening the pipe mas_pid_cmd_ins from master"); // checking errors
+    }
+
+  fd_pid_m1 = open(mas_pid_m1_ins, O_WRONLY); // open the pipe mas_pid_m1_ins to write on it
+    if(fd_pid_m1 < 0){
+        perror("error opening the pipe mas_pid_m1_ins from master"); // checking errors
+    }
+  
+  fd_pid_m2 = open(mas_pid_m2_ins, O_WRONLY); // open the pipe mas_pid_m2_ins to write on it
+    if(fd_pid_m2 < 0){
+        perror("error opening the pipe mas_pid_m2_ins from master"); // checking errors
+    }
+  
+  // save the pid dimension of the processes in the relative array to send them:
+  cmd_dim[] = sizeof(pid_cmd);
+  m1_dim[] = sizeof(pid_m1);
+  m2_dim[] = sizeof(pid_m2);
+  // save the pid of the processes in the relative array to send them:
+  cmd[] = pid_cmd;
+  m1[] = pid_m1;
+  m2[] = pid_m2;
+  
+  // send the pid dimension to the inspect:
+  if(write(fd_pid_cmd_dim, cmd_dim, sizeof(cmd_dim)) != sizeof(cmd_dim)) { // writing the pid dimension on the pipe
+    perror("error tring to write on the pid_cmd_dim pipe from master"); // checking errors
+  }
+
+  if(write(fd_pid_m1_dim, m1_dim, sizeof(m1_dim)) != sizeof(m1_dim)) { // writing the pid dimension on the pipe
+    perror("error tring to write on the pid_m1_dim pipe from master"); // checking errors
+  }
+
+  if(write(fd_pid_m2_dim, m2_dim, sizeof(m2_dim)) != sizeof(m2_dim)) { // writing the pid dimension on the pipe
+    perror("error tring to write on the pid_m2_dim pipe from master"); // checking errors
+  }
+  
+  // send the pid to the inspect:
+  if(write(fd_pid_cmd, cmd, sizeof(cmd)) != sizeof(cmd)) { // writing the pid on the pipe
+    perror("error tring to write on the pid_cmd pipe from master"); // checking errors
+  }
+
+  if(write(fd_pid_m1, m1, sizeof(m1)) != sizeof(m1)) { // writing the pid on the pipe
+    perror("error tring to write on the pid_m1 pipe from master"); // checking errors
+  }
+
+  if(write(fd_pid_m2, m2, sizeof(m2)) != sizeof(m2)) { // writing the pid on the pipe
+    perror("error tring to write on the pid_m2 pipe from master"); // checking errors
+  }
+  
+  
+  
 
   int status;
   waitpid(pid_cmd, &status, 0);
